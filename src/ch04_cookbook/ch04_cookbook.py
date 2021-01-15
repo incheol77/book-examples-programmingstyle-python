ab5d51b1fd28ff0c9283dcf42bbb7a7a6197de4b
incheol77 <incheol77@gmail.com>
2021-01-15 19:00:07 +0900

    feature : add example of ch04 cookbook
    - add the program written by traditional structured programming style, non
    idempotent

--- src/ch04_cookbook/ch04_cookbook.py
+++ src/ch04_cookbook/ch04_cookbook.py
@@ -1,7 +1,69 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import sys, string
 
-def main():
-	print("test")
+data = []
+words = []
+word_freqs = []
 
-#############
-main()
+def read_file(path_to_file):
+    global data
+    with open(path_to_file) as f:
+        data = data + list(f.read())
+
+def filter_chars_and_normalize():
+    global data
+    for i in range(len(data)):
+        if not data[i].isalnum():
+            data[i] = ' '
+        else:
+            data[i] = data[i].lower()
+
+def scan():
+    global data
+    global words
+    data_str = ''.join(data)
+    words = words + data_str.split()
+
+
+def remove_stop_words():
+    global words
+    with open('../../sample_data/stop_words.txt') as f:
+        stop_words = f.read().split(',')
+    stop_words.extend(list(string.ascii_lowercase))
+    indexes = []
+    for i in range(len(words)):
+        if words[i] in stop_words:
+            indexes.append(i)
+    for i in reversed(indexes):
+        words.pop(i)
+
+
+def frequencies():
+    global words
+    global word_freqs
+    for w in words:
+        keys = [wd[0] for wd in word_freqs]
+        if w in keys:
+            word_freqs[keys.index(w)][1] += 1
+        else:
+            word_freqs.append([w, 1])
+
+def sort():
+    global word_freqs
+    word_freqs.sort(reverse=True, key=lambda x: (x[1], x[0]))
+
+#################
+# main
+read_file(sys.argv[1])
+filter_chars_and_normalize()
+scan()
+remove_stop_words()
+frequencies()
+sort()
+
+for tf in word_freqs[0:25]:
+    print(tf[0], ' - ', tf[1])
+
+
+
+
